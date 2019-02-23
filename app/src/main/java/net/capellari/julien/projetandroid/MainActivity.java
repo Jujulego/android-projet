2e36d45f359698b160bd88d53c21ad6b6a7bc5a8
Julien Capellari <julien@capellari.net>
2019-02-23 16:19:56 +0100

    Ajout du navigation component

--- app/src/main/java/net/capellari/julien/projetandroid/MainActivity.java
+++ app/src/main/java/net/capellari/julien/projetandroid/MainActivity.java
@@ -4,14 +4,37 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
-    // Méthodes
+    // Attributs
+    private NavController navController;
+
+    // Events
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         // Layout
         setContentView(R.layout.activity_main);
+        setupNavigation();
+    }
+
+    // Méthodes
+    void setupNavigation() {
+        // Configuration
+        AppBarConfiguration appBarConfig = new AppBarConfiguration.Builder(
+                R.id.fragment_main
+        ).build();
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+
+        // Ajout de la toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        NavigationUI.setupWithNavController(toolbar, navController);
     }
 }
---
+++ app/src/main/java/net/capellari/julien/projetandroid/MainFragment.java
@@ -0,0 +1,6 @@
+package net.capellari.julien.projetandroid;
+
+import androidx.fragment.app.Fragment;
+
+public class MainFragment extends Fragment {
+}
